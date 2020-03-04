a66a11ed36a83c13f1b5b9d2017375a80c4ff5e6
QIuyan Peng <nicolepeng99@gmail.com>
2020-03-04 21:44:12 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -27,7 +27,7 @@ def fourth_challenge
         {
           :name => "Steven",
           :age => "30",
-          :attitude => ""},
+          :attitude => "confused"},
           {
           :name => "",
           :age => "",
